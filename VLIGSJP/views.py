5e711a05cd9a89745325fc958eda90926558a534
LuisGxrcia <94871441+LuisGxrcia@users.noreply.github.com>
2021-12-11 06:46:54 -0800

    Catalogos en tablas y arreglo de views

--- VLIGSJP/views.py
+++ VLIGSJP/views.py
@@ -1,57 +1,67 @@
 from django.shortcuts import render
+
+# Create your views here.
 from django.http import HttpResponse
 from django.template import Template, Context
 from django.template.loader import get_template
 from VLIGSJP.models import *
 from VLIGSJP.formularios import *
 
-#---------------CATALOGOS--------------
+
+# ---------------CATALOGOS--------------
 def CatalogoJuegos(request):
-    catalogo = CatJuegos.objects.all()
-    return render(request, 'CatalogoJuegos.html', {"catalogojuegos": catalogo})
+    catalogos = CatJuegos.objects.all()
+    return render(request, 'CatalogoJuegos.html', {"catalogos": catalogos})
+
 
 def CatalogoPeliculas(request):
-    catalogo = CatPeliculas.objects.all()
-    return render(request, 'CatalogoPeliculas.html', {"catalogopeliculas": catalogo})
+    catalogop = CatPeliculas.objects.all()
+    return render(request, 'CatalogoPeliculas.html', {"catalogos": catalogop})
 
-#--------------VENTAS-----------
+
+# --------------VENTAS-----------
 def VentaJuegos(request):
     catalogo = VentasJuegos.objects.all()
-    return render(request, 'VentaJuegos.html', {"juegosvendidos": catalogo})
+    return render(request, 'VentaJuegos.html', {"catalogos": catalogo})
+
 
 def VentaPeliculas(request):
     catalogo = VentasPeliculas.objects.all()
-    return render(request, 'VentaPeliculas.html', {"peliculasvendidas": catalogo})
+    return render(request, 'VentaPeliculas.html', {"catalogos": catalogo})
 
-#---------------CATEGORIAS DE PELICULAS
+
+# ---------------CATEGORIAS DE PELICULAS
 def CategoriaPeliculas(request):
     return render(request, 'CategoriaPeliculas.html')
 
-#----------------CLASIFICACION DE JUEGOS-------------
+
+# ----------------CLASIFICACION DE JUEGOS-------------
 def ClasifiacionJuegos(request):
     return render(request, 'ClasificacionJuegos.html')
 
 
-#------------------PELICULAS---------------
+# ------------------PELICULAS---------------
 
 def CrearPelicula(request):
-    if request.method=="POST":
+    if request.method == "POST":
         formulario = FormularioPeliculas2(request.POST)
 
         if formulario.is_valid():
-            titulo= formulario.cleaned_data.get("Titulo", "")
+            titulo = formulario.cleaned_data.get("Titulo", "")
             categoria = formulario.cleaned_data.get("Categoria", "")
             descripcion = formulario.cleaned_data.get("Descripcion", "")
             duracion = formulario.cleaned_data.get("Duracion", "")
             fecha = formulario.cleaned_data.get("Fecha", "")
             precio = formulario.cleaned_data.get("Precio", "")
 
-            insert =CatPeliculas(Titulo=titulo, Categoria_id=categoria, Descripcion=descripcion,Duracion=duracion, Fecha=fecha, Precio=precio)
+            insert = CatPeliculas(Titulo=titulo, Categoria_id=categoria, Descripcion=descripcion, Duracion=duracion,
+                                  Fecha=fecha, Precio=precio)
             insert.save()
-        return render(request,"Peliculas.html")
+        return render(request, "Peliculas.html")
     else:
         formulario = FormularioPeliculas2()
-    return render(request,"CrearPelicula.html", {"form2":formulario})
+    return render(request, "CrearPelicula.html", {"form2": formulario})
+
 
 def EliminarPelicula(request):
     if request.method == "POST":
@@ -67,14 +77,14 @@ def EliminarPelicula(request):
     return render(request, "EliminarPelicula.html", {"form": formulario})
 
 
-#----------------JUEGOS------------------
+# ----------------JUEGOS------------------
 def CrearJuego(request):
     if request.method == "POST":
         formulario = FormularioJuegos2(request.POST)
 
         if formulario.is_valid():
             titulo = formulario.cleaned_data.get("Titulo", "")
-            categoria = formulario.cleaned_data.get("Categoria","" )
+            categoria = formulario.cleaned_data.get("Categoria", "")
             descripcion = formulario.cleaned_data.get("Descripcion", "")
             clasificacion = formulario.cleaned_data.get("Clasificacion", "")
             fecha = formulario.cleaned_data.get("Fecha", "")
@@ -88,27 +98,27 @@ def CrearJuego(request):
         formulario = FormularioJuegos2()
     return render(request, "CrearJuego.html", {"form2": formulario})
 
+
 def EliminarJuego(request):
-   if request.method == "POST":
-       formulario = FormularioJuegos(request.POST)
-       if formulario.is_valid():
-           titulo = formulario.cleaned_data.get("Titulo", "")
-           borrar = CatJuegos.objects.get(Titulo=titulo)
-           borrar.delete()
-       return render(request, "BorrarJuego.html")
+    if request.method == "POST":
+        formulario = FormularioJuegos(request.POST)
+        if formulario.is_valid():
+            titulo = formulario.cleaned_data.get("Titulo", "")
+            borrar = CatJuegos.objects.get(Titulo=titulo)
+            borrar.delete()
+        return render(request, "BorrarJuego.html")
 
-   else:
-       formulario = FormularioJuegos()
-   return render(request, "EliminarJuego.html", {"form": formulario})
+    else:
+        formulario = FormularioJuegos()
+    return render(request, "EliminarJuego.html", {"form": formulario})
 
-#REGISTRO PROXIMOS ESTRENOS
+
+# REGISTRO PROXIMOS ESTRENOS
 def ProximosEstrenos(request):
-    
     if request.method == "POST":
         formulario = FormularioEstrenos(request.POST)
 
         if formulario.is_valid():
-
             titulo = formulario.cleaned_data.get("Titulo", "")
             descripcion = formulario.cleaned_data.get("Descripcion", "")
             tipo = formulario.cleaned_data.get("Tipo", "")
@@ -118,11 +128,15 @@ def ProximosEstrenos(request):
     else:
         formulario = FormularioEstrenos()
     return render(request, "RegistroProximosEstrenos.html", {"form10": formulario})
-#-----CATALOGONO ESTREENOS
+
+
+# -----CATALOGONO ESTREENOS
 def CatalogoEstrenos(request):
-    catalogo = Estrenos.objects.all()
-    return render(request, 'CatalogoEstrenos.html', {"catalogoestrenos": catalogo})
-#-------VENTAS
+    estrenos = Estrenos.objects.all()
+    return render(request, 'CatalogoEstrenos.html', {"estrenos": estrenos})
+
+
+# -------VENTAS
 def RegistrarVPeliculas(request):
     if request.method == "POST":
         formulario = FormularioPeliculas3(request.POST)
@@ -135,12 +149,13 @@ def RegistrarVPeliculas(request):
             titulo = formulario.cleaned_data.get("IdPelicula", "")
 
             insert = VentasPeliculas(Precio=precio, NombreCliente=cliente, CorreoCliente=correo, FechaCompra=fecha,
-                                  Titulo_id=titulo)
+                                     Titulo_id=titulo)
             insert.save()
-        return render(request,"VPeliculas.html")
+        return render(request, "VPeliculas.html")
     else:
         formulario = FormularioPeliculas3()
     return render(request, "RegistrarVPeliculas.html", {"form3": formulario})
+
 
 def RegistrarVJuegos(request):
     if request.method == "POST":
@@ -156,15 +171,16 @@ def RegistrarVJuegos(request):
             insert = VentasJuegos(Precio=precio, NombreCliente=cliente, CorreoCliente=correo, FechaCompra=fecha,
                                   Titulo_id=titulo)
             insert.save()
-        return render (request, "VJuegos.html")
+        return render(request, "VJuegos.html")
     else:
         formulario = FormularioJuegos3()
     return render(request, "RegistrarVJuegos.html", {"form3": formulario})
 
-#PARA MODIFICAR PAGINA DE ERRORES
-def error404(request,exeption):
+
+# PARA MODIFICAR PAGINA DE ERRORES
+def error404(request, exeption):
     return render(request, "404.html")
+
 
 def error500(request):
     return render(request, "Error500.html")
-
